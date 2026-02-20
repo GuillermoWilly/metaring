349125578578e312cd98ef50cc7fc993d7d1f66e
Guillermo Martin <guillermo.martin@dtn.com>
2026-02-20 22:29:28 +0200

    Added API credentials and function GET METAR

--- metaring/myapp/models.py
+++ metaring/myapp/models.py
@@ -5,3 +5,4 @@ from django.db import models
 class TodoItems(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
+
--- metaring/myapp/views.py
+++ metaring/myapp/views.py
@@ -1,8 +1,40 @@
 from django.shortcuts import render, HttpResponse
-from .models import TodoItems
+from .models import *
+from .utils import get_metar_from_icao
+
+
 def home(request):
-    return render(request, "home.html")
+    metar_data = get_metar_from_icao("LEZL")  # ejemplo Sevilla
+    
+    context = {
+        "metar": metar_data
+    }
+    
+    return render(request, "home.html", context)
 
 def todos(request):
     items = TodoItems.objects.all()
     return render(request, "todos.html", {"todos": items})
+
+
+
+
+
+
+
+
+
+'''class SearchView(ListView):
+    model = Airport
+    template_name = 'search.html'
+    context_object_name = 'all_search_results'
+
+    def get_queryset(self):
+       result = super(SearchView, self).get_queryset()
+       query = self.request.GET.get('search')
+       if query:
+          postresult = Airport.objects.filter(title__contains=query)
+          result = postresult
+       else:
+           result = None
+       return result'''
